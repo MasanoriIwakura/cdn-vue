91f1e1d96a6507a5ba5c8fe7a20f82a345bb3f78
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-07-20 16:43:06 +0900

    Update pages
    FormとTableのサンプルを作成

--- js/pages/one.js
+++ js/pages/one.js
@@ -1,7 +1,23 @@
 const one = {
   template: `
     <div id="one">
-      <h1>One</h1>
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Name">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Comment</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Comment"></textarea>
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <button class="button is-primary">Submit</button>
+        </div>
+      </div>
     </div>
   `
 }
--- js/pages/two.js
+++ js/pages/two.js
@@ -1,7 +1,30 @@
 const two = {
+  data: function () {
+    return {
+      items: items
+    }
+  },
   template: `
     <div id="two">
-      <p>Two</p>
+      <table class="table is-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Comment</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items">
+            <td>{{ item.name }}</td>
+            <td>{{ item.comment }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   `
 }
+
+const items = Array(10).fill().map((_, i) => ({
+  name: `name${i}`,
+  comment: `comment${i}`
+}))
