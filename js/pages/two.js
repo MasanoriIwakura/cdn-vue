0c871ca9d1f971e1976fc2b3d46b17c533c67879
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-07-21 23:31:40 +0900

    Add delete action

--- js/pages/two.js
+++ js/pages/two.js
@@ -1,7 +1,13 @@
 const two = {
   data: function () {
     return {
-      items: items
+      items: items,
+      test: [{name0: 'test'}, {name1: 'sample'}]
+    }
+  },
+  methods: {
+    remove: function (index) {
+      this.items.splice(index, 1);
     }
   },
   template: `
@@ -11,17 +17,21 @@ const two = {
           <tr>
             <th>Name</th>
             <th>Comment</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in items">
+          <tr v-for="(item, index) in items">
             <td>{{ item.name }}</td>
             <td>{{ item.comment }}</td>
+            <td>
+            <a class="button is-danger" @click="remove(index)">Delete</a>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
-  `
+  ` 
 }
 
 const items = Array(10).fill().map((_, i) => ({
